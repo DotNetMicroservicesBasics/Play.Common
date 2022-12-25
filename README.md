80f0e37518fdb7c977edf8d3a1e340a9b1ec08c5
yasserMokh <yasser.moukhtar@outlook.com>
2022-12-25 18:58:22 +0200

    added cosmosdb support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Common libraries used by play economy services.
 
 ## Create and publish package
 ```powershell
-$version="1.0.11"
+$version="1.0.12"
 $owner="DotNetMicroservicesBasics"
 $local_packages_path="D:\Dev\NugetPackages"
 $gh_pat="PAT HERE"
--- src/Play.Common/Settings/MongoDbSettings.cs
+++ src/Play.Common/Settings/MongoDbSettings.cs
@@ -2,12 +2,25 @@ namespace Play.Common.Settings
 {
     public class MongoDbSettings
     {
+
+        private string _connectionString;
         public string Host { get; init; }
         public int Port { get; init; }
 
         public string DbName { get; init; }
 
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        //public string ConnectionString => $"mongodb://{Host}:{Port}";
+
+
+        public string ConnectionString
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(_connectionString) ? $"mongodb://{Host}:{Port}" : _connectionString;
+            }
+            init { _connectionString = value; }
+        }
+
 
     }
 }
