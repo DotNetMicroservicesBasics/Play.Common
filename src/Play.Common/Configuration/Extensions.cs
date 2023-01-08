aa320ce66ea05f017eb72ccfaecb354ada539df2
yasserMokh <yasser.moukhtar@outlook.com>
2023-01-08 16:31:05 +0200

    added ConfigureAzureKeyVault extension for WebApplicationBuilder

--- src/Play.Common/Configuration/Extensions.cs
+++ src/Play.Common/Configuration/Extensions.cs
@@ -1,4 +1,5 @@
 using Azure.Identity;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Play.Common.Settings;
@@ -15,17 +16,27 @@ namespace Play.Common.Configuration
             return builder.AddAzureKeyVault(
                                 new Uri($"https://{serviceSettings.KeyVaultName}.vault.azure.net/"),
                                 new DefaultAzureCredential()
-                                );            
+                                );
         }
 
-        public static IHostBuilder ConfigureAzureKeyVault(this IHostBuilder hostBuilder){
+        public static IHostBuilder ConfigureAzureKeyVault(this IHostBuilder hostBuilder)
+        {
             return hostBuilder.ConfigureAppConfiguration((context, configurationBuilder) =>
                 {
                     if (context.HostingEnvironment.IsProduction())
                     {
                         configurationBuilder.ConfigureAzureKeyVault();
                     }
-                });                
+                });
+        }
+
+        public static WebApplicationBuilder ConfigureAzureKeyVault(this WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsProduction())
+            {
+                builder.Configuration.ConfigureAzureKeyVault();
+            }
+            return builder;
         }
     }
 }
